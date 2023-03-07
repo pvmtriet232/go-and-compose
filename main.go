ebb506cc426c2860977f43b0e829505ed0f5381f
triet <pvmtriet232@gmail.com>
2023-03-07 21:44:25 +0700

    Using API server

--- main.go
+++ main.go
@@ -1,13 +1,60 @@
 package main
 
 import (
-	"fmt"
-	"time"
+	"os"
+	"os/signal"
+	"syscall"
+
+	// Make sure you change this line to match your module
+	"github.com/bobbytables/go-and-compose/apiserver"
+	"github.com/sirupsen/logrus"
+	"github.com/urfave/cli/v2"
+)
+
+const (
+	apiServerAddrFlagName string = "addr"
 )
 
 func main() {
-	for {
-		fmt.Println("Hello World!")
-		time.Sleep(time.Second * 3)
+	if err := app().Run(os.Args); err != nil {
+		logrus.WithError(err).Fatal("could not run application")
+	}
+}
+
+func app() *cli.App {
+	return &cli.App{
+		Name:  "api-server",
+		Usage: "The API",
+		Commands: []*cli.Command{
+			apiServerCmd(),
+		},
+	}
+}
+
+func apiServerCmd() *cli.Command {
+	return &cli.Command{
+		Name:  "start",
+		Usage: "starts the API server",
+		Flags: []cli.Flag{
+			&cli.StringFlag{Name: apiServerAddrFlagName, EnvVars: []string{"API_SERVER_ADDR"}},
+		},
+		Action: func(c *cli.Context) error {
+			done := make(chan os.Signal, 1)
+			signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+
+			stopper := make(chan struct{})
+			go func() {
+				<-done
+				close(stopper)
+			}()
+
+			addr := c.String(apiServerAddrFlagName)
+			server, err := apiserver.NewAPIServer(addr)
+			if err != nil {
+				return err
+			}
+
+			return server.Start(stopper)
+		},
 	}
 }
